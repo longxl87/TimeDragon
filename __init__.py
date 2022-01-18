0c38b0c7e462a461ac3c34f7d48f5f2be437dd82
331942615@qq.com <Lxl2071828!>
2022-01-18 23:30:58 +0800

    封装核心工具类和文件，顺便添加测试数据

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .feature import (chi2_bin, best_ks_bin, make_bin, feature_analysis,calc_bin_cond)
+from .deprecated_code import (chi2_bin, best_ks_bin, make_bin, feature_analysis, calc_bin_cond)
