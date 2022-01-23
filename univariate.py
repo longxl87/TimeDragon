f7a8d227a1a7c3a1a10ddc1271899cbcd3e4270c
331942615@qq.com <Lxl2071828!>
2022-01-23 23:46:24 +0800

    封装核心工具类和文件，顺便添加测试数据

--- univariate.py
+++ univariate.py
@@ -15,6 +15,7 @@ from abc import ABCMeta
 from sklearn.utils.multiclass import type_of_target
 from pandas.api.types import is_numeric_dtype
 import warnings
+import time
 
 
 class Discretization(metaclass=ABCMeta):
@@ -32,7 +33,7 @@ class Discretization(metaclass=ABCMeta):
     """
 
     def __init__(self, max_bin=12, init_thredhold=100, init_method='qcut', precision=3, print_process=False,
-                 positive_label=1, negative_label=0):
+                 positive_label=1, negative_label=0, max_bestDsct_bin=4):
         self.max_bin = max_bin
         self.init_thredhold = init_thredhold
         self.init_method = init_method
@@ -41,58 +42,129 @@ class Discretization(metaclass=ABCMeta):
         self.positive_label = positive_label
         self.negative_label = negative_label
 
-    def _init_data(self, dat, var_name, target):
+    @abstractmethod
+    def dsct(self, dat: pd.DataFrame, var_name: str, target: str):
+        """
+        抽象接口，定义分箱方法的名称和入参，只能调用子类实现
+        Parameters
+        ----------
+        dat: 数据集，格式必须为pd.DataFrame
+        var_name:  待分箱的因变量
+        target: 目标变量
+        """
+        raise NotImplementedError("该方法只为定义基本的函数接口，不可直接调用，请使用子类实现的方法")
+
+    def _init_data(self, data, var_name, target):
         """
         init the input：
         1、校验自变量和因变量的类型
         2、判断x是否为数值类型
         3、初始化数据结果
         """
-        assert var_name in dat.columns, "数据中不包含变量%s，请检查数据" % (var_name)
-        assert var_name == target, "因变量和自变量必须是不同的变量"
+        # assert var_name in dat.columns, "数据中不包含变量%s，请检查数据" % (var_name)
+        # assert var_name != target, "因变量和自变量必须是不同的变量"
+        #
+        # dat = dat[[var_name, target]].copy()
+        #
+        # self._y_check(dat[target])
+        #
+        # is_numeric = is_numeric_dtype(dat[var_name])
+        #
+        # # unique = self.unique_noNA(dat[var_name])
+        #
+        # if is_numeric:
+        #     dat[var_name] = pd.qcut(dat[var_name], self.init_thredhold, duplicates="drop", precision=self.precision)
+        #     dti = pd.crosstab(dat[var_name], dat[target], dropna=True)
+        #     dti["positive_rate"] = dti[self.positive_label] / dti.sum(axis=1)
+        #     dti["bin"] = dti.index.map(lambda x: x.right)
+        #     mapping = None
+        # else:
+        #     dti = pd.crosstab(dat[var_name], dat[target], dropna=True)
+        #     dti["positive_rate"] = dti[self.positive_label] / dti.sum(axis=1)
+        #     dti = dti.sort_values(by="positive_rate").reset_index().reset_index().rename({"index": "bin"},
+        #                                                                                  axis=1)
+        #     mapping = dti[[var_name, "bin"]].copy()
+        #
+        # return dti[["bin", self.negative_label, self.positive_label]], var_name, mapping, is_numeric
+        if self.print_process: print("开始对变量{}进行卡方分箱:".format(x, ))
 
-        dat = dat[[var_name, target]].copy()
+        time0 = time.time()
+        assert var_name in data.columns, "数据中不包含变量%s，请检查数据" % (var_name)
+        assert var_name != target, "因变量和自变量必须是不同的变量"
+        data = data[[var_name, target]].copy()
 
-        self._y_check(dat[target])
+        self._y_check(data[target])
 
-        is_numeric = is_numeric_dtype(dat[var_name])
-
-        # unique = self.unique_noNA(dat[var_name])
+        is_numeric = is_numeric_dtype(data[var_name])
 
         if is_numeric:
-            dat[var_name] = pd.qcut(dat[var_name], self.init_thredhold, duplicates="drop", precision=self.precision)
-            dti = pd.crosstab(dat[var_name], dat[target], dropna=True)
-            dti["positive_rate"] = dti[self.positive_label] / dti.sum(axis=1)
-            dti["bin"] = dti.index.map(lambda x: x.right)
+            if self.init_thredhold is not None:
+                if self.init_method == 'qcut':
+                    data.loc[:, var_name] = pd.qcut(data[var_name], self.init_thredhold, duplicates="drop",
+                                                    precision=self.precision)
+                elif self.init_method == 'cut':
+                    data.loc[:, var_name] = pd.cut(data[var_name], self.init_thredhold, duplicates="drop",
+                                                   precision=self.precision)
+                else:
+                    raise ValueError("init_method参数仅有qcut和cut两个选项")
+                data.loc[:, var_name] = data[var_name].map(lambda x: x.right).astype(float)
+            dti = pd.crosstab(data[var_name], data[target], dropna=False).reset_index()
+            dti.rename({self.negative_label: "negative", self.positive_label: "positive"}, axis=1, inplace=True)
+            dti["variable"] = dti[var_name]
             mapping = None
         else:
-            dti = pd.crosstab(dat[var_name], dat[target], dropna=True)
-            dti["positive_rate"] = dti[self.positive_label] / dti.sum(axis=1)
-            dti = dti.sort_values(by="positive_rate").reset_index().reset_index().rename({"index": "bin"},
-                                                                                         axis=1)
-            mapping = dti[[var_name, "bin"]].copy()
+            dti = pd.crosstab(data[var_name], data[target]).reset_index()
+            dti.rename({self.negative_label: "negative", self.positive_label: "positive"}, axis=1, inplace=True)
+            dti["positive_rate"] = dti["positive"] / (dti["positive"] + dti["negative"])
+            dti = dti.sort_values("positive_rate").reset_index(drop=True).reset_index()
+            dti.rename({"index": "variable"}, axis=1, inplace=True)
+            mapping = dti[[var_name, "variable", "negative", "positive"]]
+        dti = dti[["variable", "negative", "positive"]].copy()
 
-        return dti[["bin", self.negative_label, self.positive_label]], var_name, mapping, is_numeric
+        if self.print_process:
+            time1 = time.time()
+            print("==>已经完成变量初始化耗时{:.2f}s,开始处理0值".format(time1 - time0))
 
-    def _normalize_output(self, count_df, is_numeric, mapping, var_name):
+        while (len(dti) > self.max_bin) and (len(dti.query('(negative==0) or (positive==0)')) > 0):
+            dti["count"] = dti["negative"] + dti["positive"]
+            rm_bk = dti.query("(negative==0) or (positive==0)") \
+                .query("count == count.min()")
+            ind = rm_bk["variable"].index[0]
+            if ind == dti["variable"].index.max():
+                dti.loc[ind - 1, "variable"] = dti.loc[ind, "variable"]
+            else:
+                dti.loc[ind, "variable"] = dti.loc[ind + 1, "variable"]
+            dti = dti.groupby("variable")[["negative", "positive"]].sum().reset_index()
+
+        if self.print_process:
+            time2 = time.time()
+            print("==>已经完成0值处理耗时{:.2f}s,开始进行分箱迭代".format(time2 - time1))
+
+        return dti, var_name, is_numeric, mapping
+
+    def _normalize_output(self, dti, var_name, is_numeric, mapping):
         """
         根据分箱后计算出来的结果，标准化输出
         """
-        cond = list(count_df["bin"])
-        cond[len(cond) - 1] = np.inf
-        cond.insert(0, -np.inf)
-        if not is_numeric:
-            interval_index = pd.IntervalIndex.from_breaks(cond, closed='right')
-            mapping["bin1"] = mapping["bin"].map(lambda x: np.where(interval_index.contains(x))[0][0])
-            cond = pd.Series(mapping["bin1"].values, index=mapping[var_name].values).to_dict()
+        break_points = dti['variable'].copy()
+        break_points[np.where(break_points == break_points.max())[0]] = np.inf
+        break_points = np.concatenate(([-np.inf], break_points))
+
+        if is_numeric:
+            cond = list(break_points)
+        else:
+            interval_index = pd.IntervalIndex.from_breaks(break_points, closed='right')
+            mapping["bin"] = mapping["variable"].map(lambda x: np.where(interval_index.contains(x))[0][0])
+            cond = pd.Series(mapping["bin"].values, index=mapping[var_name].values).to_dict()
+
         return cond
 
-    def unique_noNA(self, x: pd.Series):
-        """
-        pandas 中存在bug，许多场景的group 和 crosstab中的dropna参数的设定会失效，
-        在分箱的过程中剔除缺失值，若要考虑缺失值的场景，请提前对数据进行fillNa操作。
-        """
-        return np.array(list(filter(lambda ele: ele == ele, x.unique())))
+    # def unique_noNA(self, x: pd.Series):
+    #     """
+    #     pandas 中存在bug，许多场景的group 和 crosstab中的dropna参数的设定会失效，
+    #     在分箱的过程中剔除缺失值，若要考虑缺失值的场景，请提前对数据进行fillNa操作。
+    #     """
+    #     return np.array(list(filter(lambda ele: ele == ele, x.unique())))
 
     def _x_check(self, dat, var_name):
         """
@@ -136,18 +208,6 @@ class Discretization(metaclass=ABCMeta):
             if item not in [0, 1]:
                 raise ValueError("y必须是二值型变量,且其元素必须是 0 1")
 
-    @abstractmethod
-    def dsct(self, dat: pd.DataFrame, var_name: str, target: str):
-        """
-        抽象接口，定义分箱方法的名称和入参，只能调用子类实现
-        Parameters
-        ----------
-        dat: 数据集，格式必须为pd.DataFrame
-        var_name:  待分箱的因变量
-        target: 目标变量
-        """
-        pass
-
     def bestDsct(self, dat: pd.DataFrame, var_name: str, target: str):
         """
         在基础分箱的方法上增加单调性校验功能
@@ -161,19 +221,20 @@ class ChiMerge(Discretization):
     """
 
     def dsct(self, dat: pd.DataFrame, var_name: str, target: str):
-        dit, var_name, mapping, is_numeric = self._init_data(dat, var_name, target)
+        dti, var_name, is_numeric, mapping = self._init_data(dat, var_name, target)
 
-        while (len(dti) > self.max_bin) and (len(dti.query('(negative==0) or (positive==0)')) > 0):
-            dti["count"] = dti["negative"] + dti["positive"]
-            rm_bk = dti.query("(negative==0) or (positive==0)") \
-                .query("count == count.min()")
-            ind = rm_bk["variable"].index[0]
-            if ind == dti["variable"].index.max():
-                dti.loc[ind - 1, "variable"] = dti.loc[ind, "variable"]
-            else:
-                dti.loc[ind, "variable"] = dti.loc[ind + 1, "variable"]
+        time0 = time.time()
+
+        while len(dti) > self.max_bin:
+            dti["chi2"] = dti.apply(lambda row: self._calc_chi2(dti, row), axis=1)
+            min_chi2_ind = dti.query("chi2 == chi2.min()").index[0]
+            dti.loc[min_chi2_ind - 1, "variable"] = dti.loc[min_chi2_ind, "variable"]
             dti = dti.groupby("variable")[["negative", "positive"]].sum().reset_index()
-        return self._normalize_output(dit, is_numeric, mapping, var_name)
+
+        if self.print_process:
+            time1 = time.time()
+            print("==>完成分箱迭代耗时{:.2f}s".format(time1 - time0))
+        return self._normalize_output(dti, var_name, is_numeric, mapping)
 
     def _calc_chi2(self, dti, row):
         ind0 = dti[dti['variable'] == row['variable']].index[0]
@@ -221,13 +282,16 @@ class Univariate(object):
         self.print_process = print_process
         self.is_numeric = True
 
-    def base_info(self, dat, cols, target):
+    def base_info(elf, dat: pd.DataFrame, var_list: list, target: str):
         pass
 
-    def IV(self, dat, var_name, target):
+    def IV(self, dat: pd.DataFrame, var_name: str, target: str):
         pass
 
-    def woe_code(self, dat, var_name, target, ):
+    def woe_code(self, dat: pd.DataFrame, var_name: str, target: str, ):
+        pass
+
+    def feature_report(self, dat: pd.DataFrame, var_list: list, target: str):
         pass
 
 
@@ -249,10 +313,13 @@ class PlotUtils(object):
 
 
 if __name__ == "__main__":
-    rs = Discretization.unique_noNA(pd.Series(np.random.randint(0, 100, 10000)))
-    print(rs)
-#     data = pd.read_excel("data/credit_data.xlsx")
-#     print(data.shape)
+    # rs = Discretization.unique_noNA(pd.Series(np.random.randint(0, 100, 10000)))
+    # print(rs)
+    data = pd.read_excel("data/credit_data.xlsx")
+    print(data.shape)
+    chiMerge = ChiMerge(print_process=True)
+    print(chiMerge.dsct(data, 'device_app_num', 'def_pd10'))
+    print(chiMerge.dsct(data, 'continent', 'def_pd10'))
 
 # bsetks = BestKS()
 # print(bsetks.precision)
